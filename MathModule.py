9c0e70b92dd768f0db5d69acf52555b0ce59a66d
07VishnuHub <07vishnuatwork@gmail.com>
2025-05-23 11:02:51 +0530

    Create MathModule.py

---
+++ MathModule.py
@@ -0,0 +1,8 @@
+#Task 2: Using the Math Module for Calculations
+
+import math
+
+number = int(input("Enter a number: "))
+print("The square of", number, "is", pow(number, 2))
+print("The natural logarithm of", number, "is", math.log(number))
+print("The sine of", number, "is", math.sin(number))
